6d0e8102196bcbb7d18031a7d8785c43837728b6
Camila Lima <camila.lima@iv2.com.br>
2020-08-20 16:38:15 -0300

    Inclusao de pagina com detalhes

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
+let xhr = new XMLHttpRequest();
+let url = "https://api.github.com/users/CamilaGodoy1509/repos";
 
-    let xhr = new XMLHttpRequest();
-    let url = "https://api.github.com/users/CamilaGodoy1509/repos";
+xhr.open("GET", url);
 
-    xhr.open("GET", url);
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
 
-    xhr.onreadystatechange = function () {
-        if (xhr.status == 200 && xhr.readyState == 4) {
-            let dadosJSONText = xhr.responseText
-            let obj = JSON.parse(dadosJSONText)
-
-            for (let i = 0; i < obj.length; i++) {
-                document.getElementById('div').innerHTML += 
-               '<section>' + "<h1 >" + "<center id='nome1'>" + obj[i].name + '</center>' + '</h1>' +
-               '<center>'+ '<span>'+obj[i].language +  '</span>'+ '</center>'+  
-               '<center>'+ "<img src='images/" + obj[i].name + ".png'>" + '</center>' +
-               '<center>' + "<a href='" +  obj[i].html_url +  "'>"  + "Link para o Repositório" + "</a>"+  '</center>' + "<br>" + "<br>" +
-               "<center>" + "<center>"+ "<a href='index2.html' onclick='salvar()'>" + "Mais Detalhes" + "</a>" + "</center>" + "</section>"
-
-            }
+        for (let i = 0; i < obj.length; i++) {
+            document.getElementById('div').innerHTML +=
+                '<section>' + "<h1>" + "<center>" + obj[i].name + '</center>' + '</h1>' +
+                '<center>' + '<span>' + obj[i].language + '</span>' + '</center>' +
+                '<center>' + "<img src='images/" + obj[i].name + ".png'>" + '</center>' +
+                '<center>' + "<a href='" + obj[i].html_url + "'>" + "Link para o Repositório" + "</a>" + '</center>' + "<br>" + "<br>" +
+                "<center>" + "<center>" + "<a href='index2.html' onclick='salvar(this)' id='" + obj[i].name + "'>" + "Mais Detalhes" + "</a>" + "</center>" + "</section>"
 
         }
 
-          
     }
 
-    xhr.send()
-
-    function salvar() {
-            var nome = document.getElementById('nome1');
-            var json_nome = JSON.stringify(nome);
-           localStorage.setItem ("nome", json_nome);
-   }
     
-   
+
+}
+
+xhr.send()
+
+function salvar(name) {
+    
+    var json_nome = JSON.stringify(name.id);
+    localStorage.setItem("nome", json_nome);
+}
--- script2.js
+++ script2.js
@@ -1,1 +1,151 @@
-let NomeRepos = JSON.parse(localStorage.getItem("nome"))
+let NomeRepositorio = JSON.parse(localStorage.getItem("nome"))
+
+let xhr = new XMLHttpRequest();
+let url = "https://api.github.com/repos/CamilaGodoy1509/" + NomeRepositorio;
+
+xhr.open("GET", url, false);
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
+
+        
+            document.getElementById('div2').innerHTML += "<div id='div3'> Nome </div><div>"  + obj.owner.login + "</div><div> <img id='img-div' src='"+ obj.owner.avatar_url + "'></div><div id='div3'>Nome do Repositório </div><div>" + NomeRepositorio + "</div><div id='div3'>Descrição </div><div>" + obj.description + "</div>" 
+               
+    }
+
+    
+
+}
+
+xhr.send()
+
+
+//Following
+let url2 = "https://api.github.com/users/CamilaGodoy1509/following";
+
+xhr.open("GET", url2, false);
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
+
+        document.getElementById('div2').innerHTML += "<div id='div3'>Seguindo </div>" + obj.length +" usuários </div>"
+               
+    }
+
+    
+
+}
+
+xhr.send()
+
+//Commits
+let url3 = "https://api.github.com/repos/CamilaGodoy1509/" + NomeRepositorio +"/commits";
+
+xhr.open("GET", url3, false);
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
+
+        document.getElementById('div2').innerHTML += "<div id='div3'>Commits</div>"
+
+        for (let i = 0; i < obj.length; i++) {
+        
+        document.getElementById('div2').innerHTML +=  obj[i].commit.message + "<br>"
+        }  
+
+       
+    }
+
+    
+
+}
+
+xhr.send()
+
+//Branches
+let url4 = "https://api.github.com/repos/CamilaGodoy1509/" + NomeRepositorio +"/branches";
+
+xhr.open("GET", url4, false);
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
+
+        document.getElementById('div2').innerHTML += "<div id='div3'>Branches</div>"
+
+        for (let i = 0; i < obj.length; i++) {
+        
+        document.getElementById('div2').innerHTML +=  obj[i].name + "<br>"
+        }  
+
+       
+    }
+
+    
+
+}
+
+xhr.send()
+
+//Languages
+let url5 = "https://api.github.com/repos/CamilaGodoy1509/" + NomeRepositorio +"/languages";
+
+xhr.open("GET", url5, false);
+
+xhr.onreadystatechange = function () {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+        let dadosJSONText = xhr.responseText
+        let obj = JSON.parse(dadosJSONText)
+
+        document.getElementById('div2').innerHTML += "<div id='div3'>Languages</div>"
+
+        for (prop in obj) {
+        
+        document.getElementById('div2').innerHTML +=  prop + "<br>"
+        }  
+
+       
+    }
+
+    
+
+}
+
+xhr.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://api.github.com/repos/MatheusVal/REPOSITORIO/languages
